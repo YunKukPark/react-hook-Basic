df4fdb6cfe2e09ad73d94952aad5fafce2891ddc
YunKukPark <a01043340999@gmail.com>
2021-11-23 17:12:53 +0900

    add semicolon

--- useInput/useTabs.js
+++ useInput/useTabs.js
@@ -1,9 +1,9 @@
 export const useTabs = (initialTab, allTabs) => {
-  if (!allTabs || !Array.isArray(allTabs)) return
+  if (!allTabs || !Array.isArray(allTabs)) return;
 
-  const [currentIndex, setCurrentIndex] = useState(initialTab)
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
   return {
     currentItem: allTabs[currentIndex],
     changeItem: setCurrentIndex,
-  }
-}
+  };
+};
